92083eb9d47c49b9b9b586cc5721bfc68c89541e
Shoki-popo-git <popotangr@gmail.com>
2021-06-30 11:14:02 +0900

    編集・削除機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    def after_sign_up_path_for(resource)
+        shifts_path(resource)
+    end
+
+    def after_sign_in_path_for(resource)
+        shifts_path(resource)
+    end
 end
--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,11 +1,18 @@
 class ShiftsController < ApplicationController
+  before_action :set_shift, {only:[:show, :edit, :update, :destroy]}
 
   def index
     @shifts = current_user.shifts.all
   end
 
+  def show
+  end
+
   def new
     @shift = current_user.shifts.new
+  end
+
+  def edit
   end
 
   def create
@@ -13,9 +20,22 @@ class ShiftsController < ApplicationController
     redirect_to shifts_path
   end
 
+  def update
+    @shift.update(shift_parameter)
+    redirect_to shifts_path
+  end
+
+  def destroy
+    @shift.destroy
+    redirect_to shifts_path
+  end
   private
 
+  def set_shift
+    @shift = current_user.shifts.find(params[:id])
+  end
+
   def shift_parameter
-    params.require(:shift).permit(:user, :date, :start_at, :end_at)
+    params.require(:shift).permit(:user, :start_time, :start_at, :end_at)
   end
 end
