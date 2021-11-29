e7cb20aef41812fd5b40f56d0759f5a361734477
Shoki-popo-git <popotangr@gmail.com>
2021-11-29 19:07:34 +0900

    テストユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+    User.create(
+        user_name: "ユーザー#{n + 1}",
+        nickname: "ユーザー#{n + 1}",
+        email: "user#{n + 1}@test.com",
+        password: "password",
+        admin_user: false,
+    )
+    User.create(
+        user_name: "管理者#{n + 1}", 
+        nickname: "管理者#{n + 1}",
+        email: "admin#{n + 11}@test.com",
+        password: "password",
+        admin_user: true,
+    )
+end
